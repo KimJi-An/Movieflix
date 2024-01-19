be76fcf690ca4eaf1bbbbb77164c1eba967c9684
KimJi-An <j0012a@naver.com>
2024-01-19 23:50:23 +0900

    movie id로 영화정보 불러오는 getMovieById 작성

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:movieflix/models/movie_detail_model.dart';
 import 'package:movieflix/models/movie_model.dart';
 
 const String baseUrl = "https://movies-api.nomadcoders.workers.dev";
@@ -18,6 +19,17 @@ class ApiService {
         movieInstances.add(MovieModel.fromJson(movie));
       }
       return movieInstances;
+    }
+    throw Error();
+  }
+
+  static Future<MovieDetailModel> getMovieById(int id) async {
+    final url = Uri.parse("$baseUrl/movie?id=$id");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final movie = jsonDecode(response.body);
+      return MovieDetailModel.fromJson(movie);
     }
     throw Error();
   }
