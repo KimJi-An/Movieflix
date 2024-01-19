555c32d5a391fc01b15d83d2c1126f59464e0e8d
KimJi-An <j0012a@naver.com>
2024-01-19 23:50:48 +0900

    필요없는 정보 삭제

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -1,12 +1,10 @@
 class MovieModel {
-  final String title, backdropPath, overview, posterPath, releaseDate;
+  final String title, backdropPath, posterPath;
   final int id;
 
   MovieModel.fromJson(Map<String, dynamic> json)
       : title = json['title'],
         backdropPath = json['backdrop_path'],
-        overview = json['overview'],
         posterPath = json['poster_path'],
-        releaseDate = json['release_date'],
         id = json['id'];
 }
